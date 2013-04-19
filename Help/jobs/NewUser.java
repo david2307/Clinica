28a46818ffe439fb43f6b2e15ee4094e90541bd8
Mario Garcia <mariog.david@gmail.com>
2013-04-18 23:48:09 -0600

    Creacion del Job y generacion del token

--- Help/jobs/NewUser.java
+++ Help/jobs/NewUser.java
@@ -1,11 +1,18 @@
 package jobs;
 
 import play.jobs.Job;
+import play.libs.Crypto;
 
 public class NewUser extends Job{
 	
-	public void doJob(){
-		
+	public NewUser(Long id){
+		doJob(id);
+	}
+	
+	public void doJob(Long id){
+		Crypto crypto = new Crypto();
+		String token = crypto.encryptAES(String.valueOf(id));
+		System.out.println(token);
 	}
 
 }
