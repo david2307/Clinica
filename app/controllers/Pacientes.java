752b3349f8fb00512f415b637098741f3ec80f2a
Alex García <lxfrnnd1988@gmail.com>
2013-04-18 23:12:47 -0600

    agregando busqueda y modificacion de un paciente
    controladores para hacer la busqueda y modificacion de un paciente

--- app/controllers/Pacientes.java
+++ app/controllers/Pacientes.java
@@ -43,4 +43,76 @@ public class Pacientes extends Controller {
 	public static void crearFichaMedica(){
 		
 	}
+	
+		//GET
+		public static void showBuscaPaciente(){
+			render();
+		}
+		
+		//POST 
+		public static void buscaPaciente(@Required(message="El dato para la busqueda es requerido") String busqueda){
+			if (validation.hasErrors()){
+				render("Pacientes/showBuscaPaciente",busqueda);
+			}
+			long idPaciente = Long.parseLong(busqueda);
+			
+			Paciente paciente = Paciente.find("Select p From Paciente p where p.nombre like ? or p.apellido like ? or p.id ==?", busqueda, busqueda, idPaciente).first();
+			
+			if(paciente !=null){
+				render("Paciente/modificaPaciente.html",paciente);
+			}else{
+				validation.equals(paciente,null).message("Error, no se encontro el paciente");
+				render("Paciente/showBuscPaciente.html",busqueda);
+			}
+		}
+		
+		
+		//POST
+		public static void modificaPaciente(@Required(message="El paciente es requerido") long idPaciente,
+											@Required(message="El nombre es requerido") String nombre,
+											@Required(message="El apellido es requerido") String apellido,
+											@Required(message="La fecha de nacimiento es requerida") Date fechanac,
+											@Required(message="El sexo es requerido") String sexo,
+											@Required(message="La direccion es requerido") String direccion,
+											@Required(message="El telefono es requerido") int telefono,
+											String dpi, String nombreEmergencia, int telefonoEmergencia,
+											@Required(message="El municipio es requerido") Municipio municipio,
+											Usuario usuario, Date ultimaVisita, String requerido, String observaciones){
+			if (validation.hasErrors()){
+				render(idPaciente, nombre, apellido, fechanac, sexo, direccion, telefono, dpi, nombreEmergencia, telefono, dpi, nombreEmergencia,
+						telefonoEmergencia, municipio, usuario, ultimaVisita, requerido, observaciones);
+			}
+			
+			Paciente paciente = Paciente.findById(idPaciente);
+			if(paciente != null){
+				paciente.nombre = nombre;
+				paciente.apellido = apellido;
+				paciente.fechaNac = fechanac;
+				paciente.sexo = sexo;
+				paciente.telefono = telefono;
+				paciente.dpi = dpi;
+				paciente.nombreEmergencia =nombreEmergencia;
+				paciente.telefonoEmergencia = telefonoEmergencia;
+				paciente.municipio = municipio;
+				paciente.usuario = usuario; 
+				paciente.ultimaVisita = ultimaVisita;
+				paciente.referido = requerido;
+				paciente.observaciones = observaciones;
+				
+				if(paciente.validateAndSave()){
+					if(paciente.validateAndSave()){
+						flash.success("El paciente se modificado exitosamente");
+						render("Paciente/show.hml");					
+					}else{
+						validation.equals(paciente,!paciente.validateAndSave()).message("Error, no se pudo modificar el paciente");
+						render("Paciente/show.html");
+					}
+				}
+			}else{
+				validation.equals(paciente,null).message("Error, no se encontro el paciente");
+				render("Paciente/showBuscPaciente.html");
+			}
+			
+			
+		}
 }
