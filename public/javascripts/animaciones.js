0ec04010c688bb185bd4bf4c178d21035ffe56e0
Mario Garcia <mariog.david@gmail.com>
2013-04-22 15:13:16 -0600

    Agregando templates y dependencias

---
+++ public/javascripts/animaciones.js
@@ -0,0 +1,12 @@
+$(function()
+	{
+		$('#button-notification').popover();
+	});
+/*
+$(document).ready(function () {
+$("#button-menu-slider").click(function () { 
+      $("#menu-options").addClass(".muestraMenu");
+});
+});*/
+
+
