eb2ca7cb34ca78c220770363c76aa41b81f15de7
Alex García <lxfrnnd1988@gmail.com>
2013-04-23 22:19:48 -0600

    Usuarios, Modificar usuario
    agregando la modificacion de usuarios y guardando el tipo de usuario
    cuando este se crea

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -7,6 +7,7 @@ import models.Municipio;
 import models.Paciente;
 import models.Pregunta;
 import models.Usuario;
+import models.TipoUsuario;
 
 import play.data.validation.Required;
 import play.libs.Crypto;
@@ -22,8 +23,9 @@ public class Usuarios extends Controller {
 		Crypto crypto = new Crypto();
 		long idUsuario = Long.valueOf(crypto.decryptAES(token));
 		Paciente paciente = Paciente.findById(idUsuario);
+		List<TipoUsuario> tiposUsuario = TipoUsuario.findAll();
 		if(paciente != null){
-			render(paciente);
+			render(paciente, tiposUsuario);
 		}else{
 			render("Pacientes/show.html");
 		}
@@ -32,7 +34,8 @@ public class Usuarios extends Controller {
 	//POST
 	public static void crearUsuario(@Required(message="El nickName es requerido")String nickName, 
 			                        @Required(message="El password es requerido")String password,
-			                        @Required(message="El paciente es requerido")Long paciente){
+			                        @Required(message="El paciente es requerido")Long paciente,
+			                        @Required(message="El tipo de usuario es requerido") TipoUsuario tipoUsuario){
 		
 		if(validation.hasErrors()){
 			render("Pacientes/crearUsuario",nickName,password,paciente);
@@ -42,7 +45,7 @@ public class Usuarios extends Controller {
 		if(usuario == null){
 			Crypto crypto = new Crypto();
 			String pass = crypto.encryptAES(String.valueOf(password));
-			Usuario crearUsuario = new Usuario(nickName,pass,true,null).save();
+			Usuario crearUsuario = new Usuario(nickName,pass,true,tipoUsuario).save();
 			
 			if(crearUsuario !=null){
 				Paciente buscaPaciente = Paciente.findById(paciente);
@@ -52,19 +55,52 @@ public class Usuarios extends Controller {
 					buscaPaciente.usuario = crearUsuario;
 					buscaPaciente.validateAndSave();
 					Security.authenticate(nickName, password);
-					Security.onAuthenticated();
-					
 				}else{
 					validation.equals(buscaPaciente,null).message("Error, no se encontro el paciente");
-					render("Pacientes/crearUsuario",nickName,password,paciente);
+					render("Usuarios/crearUsuario",nickName,password,paciente);
 				}
 			}else{
 				validation.equals(crearUsuario,null).message("Error, no se pudo crear el usuario");
-				render("Pacientes/crearUsuario",nickName,password,paciente);
+				render("Usuarios/crearUsuario",nickName,password,paciente);
 			}
 		}else{
 			validation.equals(usuario,null).message("Error, el nickName ya está registrado");
-			render("Pacientes/crearUsuario",nickName,password,paciente);
+			render("Usuarios/crearUsuario",nickName,password,paciente);
 		}
 	}
+	
+	//GET 
+	public static void mostrarModificarUsuario(){
+		Usuario usuario = Usuario.find("byNickName", Security.connected()).first();
+		if(usuario != null){
+			render(usuario);
+		}
+	}
+	
+	//POST
+	public static void modificarUsuario(@Required(message="El nickName es requerido") String nickName, 
+										@Required(message="El password es requerido") String password,
+										@Required(message="El estado es requerido")  boolean estado,
+										@Required(message="El tipo de usuario es requerido") TipoUsuario tipoUsuario ){
+		if(validation.hasErrors()){
+			render("Usuarios/mostraModificarUsuario",nickName,password,estado);
+		}
+		Usuario usuario = Usuario.find("byNickName", nickName).first();
+		
+		if(usuario != null){
+			usuario.password = password;
+			usuario.status =estado;
+			usuario.tipoUsuario = tipoUsuario;
+			
+			if(usuario.validateAndSave()){
+				flash.success("Usuario actualizado correctamente");
+				Security.onAuthenticated();
+			}
+		}else{
+			validation.equals(usuario,null).message("Error, no se encontro el usuario");
+			render("Usuarios/modificarUsuario",nickName,password,estado,tipoUsuario);
+		}
+		
+	}
+	
 }
